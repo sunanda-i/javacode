befbcf91d326ee6c3faeb0f979a95f8832122577
sunanda-i <sunanda.ivatury@gmail.com>
2026-01-06 16:41:28 +0530

    Login To Amazon

---
+++ src/test/java/package1/Project1/TestCase1_LoginToAmazon.java
@@ -0,0 +1,39 @@
+package package1.Project1;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+
+import utility.BaseClass;
+import utility.ExcelUtil;
+
+public class TestCase1_LoginToAmazon extends BaseClass {
+	@DataProvider(name = "LoginData")
+    public Object[][] getData() throws Exception {
+    	String path = System.getProperty("user.dir")
+                + "/Excelsheet/Testdata.xlsx";
+        return ExcelUtil.getExcelData(path, "validcredentials");
+    }
+	
+	@Test(dataProvider = "LoginData")
+	public void withValidCredentials(String Username, String Password) throws InterruptedException
+	{
+		HomePage homepage = new HomePage(driver);
+		
+		homepage.hoverOveronAccountList(driver);
+		homepage.clickToSignIn(driver);
+		
+    	LoginPage loginpage =new LoginPage(driver);
+		loginpage.enterUsername(Username);
+		loginpage.clickOnContinue();
+		loginpage.enterPassword(Password);
+		loginpage.clickonSigin();
+		
+		
+		
+		
+	}
+	
+
+}
